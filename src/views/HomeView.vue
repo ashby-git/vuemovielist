c0acb464cf923da8dcf7d473f8d02485d44662b2
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-11-23 15:55:40 +0900

    ✨add HomePage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="WelcomeContainer">
+    <h1 class="Title">Welcome to MyMovieList!</h1>
+    <div class="Card">
+      <router-link class="GetStartedContainer" to="/search">
+        <h2 class="Subheading">Get Started!</h2>
+        <p class="Textbox">
+          Click here to get started with your own MovieList now.
+        </p>
+      </router-link>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  name: "HomeView",
+};
 </script>
+
+<style scoped>
+.WelcomeContainer {
+  max-width: max-content;
+  display: grid;
+  margin: 0 auto;
+  padding: 80px 0 0 0;
+}
+.Title {
+  text-align: center;
+  font-size: 3rem;
+  padding: 2rem;
+}
+.Card {
+  padding: 1rem;
+  margin: 1rem;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 6px;
+  background-color: #f1e1fc;
+}
+.GetStartedContainer {
+  cursor: pointer;
+  text-decoration: inherit;
+  color: inherit;
+}
+.Subheading {
+  text-align: center;
+  font-size: 2rem;
+  padding: 1rem;
+}
+.Textbox {
+  text-align: center;
+  font-size: 1rem;
+  padding: 1rem;
+}
+</style>
