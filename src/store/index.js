3cd10e0aacc16da9a0d12d842e4821f314f98e1d
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-11-24 01:18:03 +0900

    ✨setup vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,63 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    watchlist: [],
+    watched: [],
+    changed: false,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    pushMovieToWatchlist(state, { movie }) {
+      state.watchlist.push(movie);
+    },
+    filterMovieFromWatchlist(state, { id }) {
+      state.watchlist = state.watchlist.filter(
+        (watchlistMovie) => watchlistMovie.id !== id
+      );
+    },
+    pushMovieToWatched(state, { movie }) {
+      state.watched.push(movie);
+      state.watchlist = state.watchlist.filter(
+        (watchlistMovie) => watchlistMovie.id !== movie.id
+      );
+    },
+    filterMovieFromWatched(state, { id }) {
+      state.watched = state.watched.filter(
+        (watchedMovie) => watchedMovie.id !== id
+      );
+    },
+    transferMovieToWatchlist(state, { movie }) {
+      state.watchlist.push(movie);
+      state.watched = state.watched.filter(
+        (watchedMovie) => watchedMovie.id !== movie.id
+      );
+    },
+    setChanged(state, status) {
+      state.changed = status;
+    },
   },
   actions: {
+    addMovieToWatchlist({ commit }, movie) {
+      commit("setChanged", true);
+      commit("pushMovieToWatchlist", { movie: movie });
+    },
+    removeMovieFromWatchlist({ commit }, movie) {
+      commit("setChanged", true);
+      commit("filterMovieFromWatchlist", { id: movie.id });
+    },
+    addMovieToWatched({ commit }, movie) {
+      commit("setChanged", true);
+      commit("pushMovieToWatched", { movie: movie });
+    },
+    removeMovieFromWatched({ commit }, movie) {
+      commit("setChanged", true);
+      commit("filterMovieFromWatched", { id: movie.id });
+    },
+    moveMovieToWatchlist({ commit }, movie) {
+      commit("setChanged", true);
+      commit("transferMovieToWatchlist", { movie: movie });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
